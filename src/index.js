7a3c76317cd896842e8f7b7d201cb785ea54fef6
Hideo Tommaso Nishimura <Hideo@Hideos-MacBook-Air.local>
2022-02-05 17:04:43 +0100

    Create html input form in JXS

--- src/index.js
+++ src/index.js
@@ -1,15 +1,24 @@
 // Import the React and ReactDOM libraries
 
-import React from 'react';
-import ReactDOM from 'react-dom';
+import React from "react";
+import ReactDOM from "react-dom";
 
-// Create a React component 
+// Create a React component
 
 const App = () => {
-    return <div>Fruf</div>;
+  return (
+    <div>
+      <label class="label" for="name">
+        Enter Name
+      </label>
+      <input id="name" type="text" />
+      <button style={{backgroundColor: 'black', color: 'white'}}>
+          Submit
+      </button>
+    </div>
+  );
 };
-
 
 // Take the React component and show it on the screen
 
-ReactDOM.render(<App />, document.querySelector('#root'));
+ReactDOM.render(<App />, document.querySelector("#root"));
